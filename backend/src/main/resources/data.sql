607d5bcb124f6e9f8bd7d9f0feda64a468809a12
jracchumi2014-sys <jracchumi2014@gmail.com>
2026-06-06 03:07:55 -0500

    Add non-admin seeded user estudiante to H2 data.sql

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,4 +1,5 @@
 INSERT INTO roles (name) VALUES ('ADMIN'), ('ALMACENERO'), ('COORDINADOR'), ('DOCENTE'), ('ESTUDIANTE');
 
 INSERT INTO usuarios (username, password, fullname, role_id)
-VALUES ('admin', '$2b$12$SaaHWfgBkuO2Q7Ebjyd9xOz1IFVivh63UGLh1BEyanXCmZ2g0Qfam', 'Administrador Principal', 1);
+VALUES ('admin', '$2b$12$SaaHWfgBkuO2Q7Ebjyd9xOz1IFVivh63UGLh1BEyanXCmZ2g0Qfam', 'Administrador Principal', 1),
+       ('estudiante', '$2b$12$Dqhv1BrRiQBtXS8tsx2WrOq3e5/hMYI5tJEzjeu84yy4hmrc7OJh6', 'Estudiante Ejemplo', 5);
